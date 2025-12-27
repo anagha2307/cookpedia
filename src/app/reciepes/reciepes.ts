51bfca28c680cbce352b90a2f46be9630a18aa6e
anagha2307 <anaghavarghese100@gmail.com>
2025-12-27 23:49:12 +0530

    Cookpedia Day4 : Recipe loading with api

--- src/app/reciepes/reciepes.ts
+++ src/app/reciepes/reciepes.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Header } from '../header/header';
 import { Footer } from '../footer/footer';
+import { ApiService } from '../services/api-service';
 
 @Component({
   selector: 'app-reciepes',
@@ -9,5 +10,15 @@ import { Footer } from '../footer/footer';
   styleUrl: './reciepes.css',
 })
 export class Reciepes {
-
+  allRecipes :any = []
+  api = inject(ApiService)
+  ngOnInit(){
+    this.getAllRecipes()
+  }
+  getAllRecipes(){
+    this.api.getAllRecipesAPI().subscribe((res :any) => {
+      this.allRecipes = res
+      console.log(this.allRecipes);
+    })
+  }
 }
--- src/app/view-reciepe/view-reciepe.ts
+++ src/app/view-reciepe/view-reciepe.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Header } from '../header/header';
+import { Footer } from '../footer/footer';
 
 @Component({
   selector: 'app-view-reciepe',
-  imports: [],
+  imports: [Header,Footer],
   templateUrl: './view-reciepe.html',
   styleUrl: './view-reciepe.css',
 })
