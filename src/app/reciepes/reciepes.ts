672667f55f1362fc12aaf82c831b0e9f118f672f
anagha2307 <anaghavarghese100@gmail.com>
2025-12-28 23:10:49 +0530

    Cookpedia Day5 : View Recipe

--- src/app/reciepes/reciepes.ts
+++ src/app/reciepes/reciepes.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { Header } from '../header/header';
 import { Footer } from '../footer/footer';
 import { ApiService } from '../services/api-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reciepes',
@@ -10,15 +11,51 @@ import { ApiService } from '../services/api-service';
   styleUrl: './reciepes.css',
 })
 export class Reciepes {
+
   allRecipes :any = []
+  cuisineArray : any = []
+  mealTypeArray : any = []
+  dummyAllRecipe : any = []
+
   api = inject(ApiService)
+  router = inject(Router)
+
   ngOnInit(){
     this.getAllRecipes()
   }
+  //get all recipes
   getAllRecipes(){
-    this.api.getAllRecipesAPI().subscribe((res :any) => {
+    this.api.getAllRecipesAPI().subscribe((res : any) => {
       this.allRecipes = res
-      console.log(this.allRecipes);
+      //console.log(this.allRecipes);
+      this.dummyAllRecipe = res
+
+      //cuisine
+      this.allRecipes.forEach((item:any) => {
+        !this.cuisineArray.includes(item.cuisine) && this.cuisineArray.push(item.cuisine)
+      });
+      //console.log(this.cuisineArray);
+      
+      //meal type 
+      const dummyMealArray = this.allRecipes.map((item : any) => item.mealType).flat(Infinity)
+        dummyMealArray.forEach((item:any) => {
+          !this.mealTypeArray.includes(item) && this.mealTypeArray.push(item)
+        })
+      //console.log(this.mealTypeArray);
     })
+  }
+  //filter recipes
+  filterRecipe(key:string,value:string){
+    this.allRecipes = this.dummyAllRecipe.filter((item:any) => item[key] == value)
+  }
+
+  //navigate view
+  navigateView(recipeId:string){
+    if(sessionStorage.getItem("token")){
+      this.router.navigateByUrl(`recipes/${recipeId}/view`)
+    }
+    else{
+      alert('Please Login to get full access to our Recipe Collection....!!!!')
+    }
   }
 }
--- src/app/view-reciepe/view-reciepe.ts
+++ src/app/view-reciepe/view-reciepe.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from '../header/header';
 import { Footer } from '../footer/footer';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-view-reciepe',
-  imports: [Header,Footer],
+  imports: [Header, Footer, RouterLink],
   templateUrl: './view-reciepe.html',
   styleUrl: './view-reciepe.css',
 })
