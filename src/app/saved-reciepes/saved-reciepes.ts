04d4f1cd5e5150152b257f51a22eae14943c7787
anagha2307 <anaghavarghese100@gmail.com>
2025-12-28 23:58:20 +0530

    Cookpedia Day5 : Recipe filter and search

--- src/app/saved-reciepes/saved-reciepes.ts
+++ src/app/saved-reciepes/saved-reciepes.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Header } from '../header/header';
+import { Footer } from '../footer/footer';
 
 @Component({
   selector: 'app-saved-reciepes',
-  imports: [],
+  imports: [Header,Footer],
   templateUrl: './saved-reciepes.html',
   styleUrl: './saved-reciepes.css',
 })
